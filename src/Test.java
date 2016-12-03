a7e7169a00f7612e4968bffee058b5ff41c148b3
irmowan <irmowan@gmail.com>
2016-12-03 19:42:10 +0800

    Add gui AST

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,12 @@
 // Created by irmo on 16/12/3.
 
+import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.gui.TestRig;
+
+import javax.swing.*;
+import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) throws Exception {
@@ -10,6 +15,19 @@ public class Test {
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         MiniJavaParser parser = new MiniJavaParser(tokens);
         ParseTree tree = parser.goal();
+
+        // Show AST in console
         System.out.println(tree.toStringTree(parser));
+
+        // Show AST in GUI
+        JFrame frame = new JFrame("ANTLR AST");
+        JPanel panel = new JPanel();
+        TreeViewer viewer = new TreeViewer(Arrays.asList(parser.getRuleNames()), tree);
+        viewer.setScale(1.0);
+        panel.add(viewer);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 400);
+        frame.setVisible(true);
     }
 }
