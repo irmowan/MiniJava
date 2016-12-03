84ca7a392d5f0a8fac7defac479341c586a049bc
irmowan <irmowan@gmail.com>
2016-12-04 00:36:01 +0800

    Add scope tracking with braces

--- src/DefPhase.java
+++ src/DefPhase.java
@@ -20,4 +20,44 @@ public class DefPhase extends MiniJavaBaseListener {
         globals = new Block(null);
         currentScope = globals;
     }
+
+    @Override
+    public void exitProg(MiniJavaParser.ProgContext ctx) {
+        currentScope = currentScope.getOuterScope();
+    }
+
+    @Override
+    public void enterMainClass(MiniJavaParser.MainClassContext ctx) {
+        currentScope = new Block(currentScope);
+    }
+
+    @Override
+    public void exitMainClass(MiniJavaParser.MainClassContext ctx) {
+        currentScope = currentScope.getOuterScope();
+    }
+
+    @Override
+    public void enterClassDec(MiniJavaParser.ClassDecContext ctx) {
+        currentScope = new Block(currentScope);
+    }
+
+    @Override
+    public void exitClassDec(MiniJavaParser.ClassDecContext ctx) {
+        currentScope = currentScope.getOuterScope();
+    }
+    
+    @Override
+    public void enterMethodDec(MiniJavaParser.MethodDecContext ctx) {
+        currentScope = new Block(currentScope);
+    }
+
+    @Override
+    public void enterBraceStatement(MiniJavaParser.BraceStatementContext ctx) {
+        currentScope = new Block(currentScope);
+    }
+
+    @Override
+    public void exitBraceStatement(MiniJavaParser.BraceStatementContext ctx) {
+        currentScope = currentScope.getOuterScope();
+    }
 }
