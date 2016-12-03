2ccd8afab7771bf3458045ec1998fa4fc7b20a5b
irmowan <irmowan@gmail.com>
2016-12-03 19:24:16 +0800

    Add test class

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+// Created by irmo on 16/12/3.
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        ANTLRInputStream input = new ANTLRInputStream(System.in);
+        MiniJavaLexer lexer = new MiniJavaLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        MiniJavaParser parser = new MiniJavaParser(tokens);
+        ParseTree tree = parser.goal();
+        System.out.println(tree.toStringTree(parser));
+    }
+}
