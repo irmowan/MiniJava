8257fde52f0014b80f0b95494156f257e6195d4a
irmowan <irmowan@gmail.com>
2016-12-06 14:34:59 +0800

    Change file load path

--- src/Test.java
+++ src/Test.java
@@ -15,7 +15,7 @@ public class Test {
         if (args.length > 0) {
             inputFile = args[0];
         } else {
-            inputFile = "src/input/BinarySearch_Error.minijava";
+            inputFile = "input/BinarySearch.minijava";
         }
         InputStream in = new FileInputStream(inputFile);
         System.out.println("File loaded.");
